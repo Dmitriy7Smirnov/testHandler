0ffb081fcf230d9e53731ecd5417e12544c37032
dmitriy7smirnov <dmitriy7smirnov@gmail.com>
2020-06-03 11:22:26 +0300

    were divided function and datasets
    Signed-off-by: dmitriy7smirnov <dmitriy7smirnov@gmail.com>

--- lib/test.ex
+++ lib/test.ex
@@ -12,29 +12,40 @@ defmodule Test do
     end
 
     def start_tests do
-        {:ok, list_content} = File.read("items")
-        fun_map = ExJSON.parse(list_content, :to_map)
+        {:ok, functions_list} = File.read("functions")
+        fun_map = ExJSON.parse(functions_list, :to_map)
+        {:ok, datasets_list} = File.read("datasets")
+        datasets_map = ExJSON.parse(datasets_list, :to_map)
         fun_list = Map.keys(fun_map)
         IO.puts("")
         List.foldl(fun_list, 0, fn fun_name, _acc ->
-                params = fun_map[fun_name]
-                test_all_cases(fun_name, List.foldl(Map.keys(params), [], fn key, acc -> [params[key] | acc] end), [])
+                params = fun_map[fun_name]["params"]
+                fun_module = fun_map[fun_name]["module"]
+                test_all_cases(fun_module, fun_name, List.foldl(Map.keys(params), [], fn key, acc -> [datasets_map[params[key]] | acc] end), [])
                 IO.puts("")
             end);
     end
 
-    defp test_all_cases(fun_name, p_lists, args) do
+    defp test_all_cases(fun_module, fun_name, p_lists, args) do
         case length(p_lists) == 1 do
-            true -> List.foldl(List.first(p_lists), 0, fn x, _acc -> apply(__MODULE__, String.to_atom(fun_name), [x | args]) end);
-            false -> List.foldl(List.first(p_lists), 0, fn x, _acc -> test_all_cases(fun_name, List.delete_at(p_lists, 0), [x | args]) end)
+            true ->
+                List.foldl(List.first(p_lists),
+                    0,
+                    fn x, _acc ->
+                        params = [x | args]
+                        result = apply(String.to_atom("Elixir." <> fun_module), String.to_atom(fun_name), params)
+                        IO.puts("#{fun_module}.#{fun_name} params = #{Enum.join(params, " ")}; result = #{result}")
+                    end
+                );
+            false -> List.foldl(List.first(p_lists), 0, fn x, _acc -> test_all_cases(fun_module, fun_name, List.delete_at(p_lists, 0), [x | args]) end)
         end
     end
 
     def add2(x, y) do
-        IO.puts("fun add/2: #{x} + #{y} = #{x + y}")
+        x + y
     end
 
     def add3(x, y, z) do
-        IO.puts("fun add/3: #{x} + #{y} + #{z} = #{x + y + z}")
+        x + y + z
     end
 end
---
+++ lib/utils.ex
@@ -0,0 +1,5 @@
+defmodule Utils do
+    def sub2(x, y) do
+        x - y
+    end
+end
